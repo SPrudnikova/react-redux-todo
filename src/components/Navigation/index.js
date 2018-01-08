07a32405f93ae44a41186a66f46d5f02c00434f2
sprudnikava <sprudnikava@exadel.com>
2018-01-08 17:16:21 +0300

    add scss loader, absolute path resolution

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {NavLink} from "react-router-dom";
 
-import './index.css';
+import './index.scss';
 import {connect} from "react-redux";
 import {logoutUser, addNewTodo} from "../../actions";
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Root from './scenes/Root';
-import registerServiceWorker from './registerServiceWorker';
-import './App.css';
+import Root from 'scenes/Root';
+import registerServiceWorker from 'registerServiceWorker';
+import 'App.scss';
 
 ReactDOM.render(<Root/>, document.getElementById('root'));
 registerServiceWorker();
